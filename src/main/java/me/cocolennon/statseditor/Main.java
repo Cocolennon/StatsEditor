c04f0342c52e670d6c1afba170ebd68617d873ce
Cocolennon <cocolennong@gmail.com>
2024-02-13 19:36:02 +0100

    Added instance and removed static declarations

--- src/main/java/me/cocolennon/statseditor/Main.java
+++ src/main/java/me/cocolennon/statseditor/Main.java
@@ -4,16 +4,21 @@ import me.cocolennon.statseditor.commands.EditMobStatCommand;
 import me.cocolennon.statseditor.commands.EditItemStatCommand;
 import me.cocolennon.statseditor.commands.EditStatCommand;
 import me.cocolennon.statseditor.commands.StatsEditorCommand;
+import me.cocolennon.statseditor.listeners.JoinListener;
 import me.cocolennon.statseditor.utils.Logger;
+import me.cocolennon.statseditor.utils.UpdateChecker;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    private static Main instance;
     public static String version;
     public static String latestVersion;
     public static boolean usingOldVersion = false;
 
     @Override
     public void onEnable() {
+        instance = this;
         //checkVersion();
         registerCommands();
         Logger.log("&aPlugin enabled!");
@@ -24,26 +29,34 @@ public class Main extends JavaPlugin {
     }
 
     /*public void checkVersion() {
-        new UpdateChecker(this, this, 105225).getVersion(cVersion -> {
+        new UpdateChecker(this, this, 111167).getVersion(cVersion -> {
             version = this.getDescription().getVersion();
             latestVersion = cVersion;
             if (!getVersion().equals(cVersion)) {
-                getLogger().info("You are using an older version of Animated Nicknames, please update to version " + cVersion);
+                getLogger().info("You are using an older version of Stats Editor, please update to version " + cVersion);
                 usingOldVersion = true;
             }
         });
     }*/
 
-    public void registerCommands() {
+    private void registerCommands() {
         getCommand("statseditor").setExecutor(new StatsEditorCommand());
         getCommand("edit-stat").setExecutor(new EditStatCommand());
         getCommand("edit-item-stat").setExecutor(new EditItemStatCommand());
         getCommand("edit-mob-stat").setExecutor(new EditMobStatCommand());
     }
 
-    public static String getVersion() { return version; }
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+    }
 
-    public static String getLatestVersion(){ return latestVersion; }
+    public String getVersion() { return version; }
 
-    public static boolean getUsingOldVersion() { return usingOldVersion; }
+    public String getLatestVersion(){ return latestVersion; }
+
+    public boolean getUsingOldVersion() { return usingOldVersion; }
+
+    public static Main getInstance() {
+        return instance;
+    }
 }
